406b7445afe8a0889b891c1f0779aa5b53aa4f63
Farid <difcortesgu@unal.edu.co>
2020-06-08 16:21:33 -0500

    success

---
+++ src/test/java/co/edu/unal/software_engineering/labs/service/SuccessTest.java
@@ -0,0 +1,21 @@
+package co.edu.unal.software_engineering.labs.service;
+
+import co.edu.unal.software_engineering.labs.model.Course;
+import co.edu.unal.software_engineering.labs.pojo.CoursePOJO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith( MockitoJUnitRunner.class)
+public class SuccessTest{
+
+    @Test
+    public void mapperCourseEntityTest( ){
+        assertEquals(1,1);
+    }
+
+}
